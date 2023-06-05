980279e7d016dd7db8ad0563147c72e9a34097b9
srt1017 <omarfarouq107@gmail.com>
2023-06-05 12:35:31 -0500

    initial commit

--- fitnesslandingpage/tailwind.config.js
+++ fitnesslandingpage/tailwind.config.js
@@ -16,7 +16,22 @@ module.exports = {
     backgroundImage : (theme) => ({
       "gradient-yellowred": "linear-gradient(90deg, #ff616a 0%, #ffc837 100%)",
       "mobile-home": "url('./assets/HomePageGraphic.png')"
-    })
+    }),
+    fontFamily: {
+      dmsans: ["DM Sans", "sans-serif" ],
+      montserrat: ["Montserrat", "sans-serif"]
+    },
+    content: {
+      evolvetext: "url('./assets/EvolveText.png')",
+      abstractwaves: "url('./assets/AbstractWaves.png')",
+      sparkles: "url('./assets/Sparkles.png')",
+      circles: "url('./assets/Circles.png')",
+    }
+  },
+  screens: {
+    xs: "480px",
+    sm: "768px",
+    md: "1060px"
   },
   plugins: [],
 }
